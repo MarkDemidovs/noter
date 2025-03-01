cba834fc5e0c30951f9801fe98b9ae72ca20f43e
Mark <markdemidovs@proton.me>
2025-03-02 00:54:08 +0200

    finally fixed the issues with permissions, now fully working

--- server/db.js
+++ server/db.js
@@ -6,7 +6,7 @@ const pool = new Pool({
     host: process.env.DB_HOST,
     database: process.env.DB_NAME,
     password: process.env.DB_PASS,
-    port: 5000,
+    port: 5432,
 })
 
 module.exports = pool;
